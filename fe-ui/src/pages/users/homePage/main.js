6130ef0f8a1d74cf95e647bc21de14d58ab2d886
Thành Đạt <103475806+datkala12@users.noreply.github.com>
2024-02-22 10:08:26 +0700

    upload src

---
+++ fe-ui/src/pages/users/homePage/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'daisyui/dist/full.css';
+
+export default function index() {
+    return <p>Xin chào</p>;
+    // (
+    //     <div>
+
+    //         <input type="checkbox" value="synthwave" class="toggle theme-controller" />
+    //     </div>
+    // )
+}
